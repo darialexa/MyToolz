d433e57bdc84f8365df78f6eaa87736619f797ab
darialexa <110948671+darialexa@users.noreply.github.com>
2022-09-04 14:15:54 -0700

    Update init.py
    Initial init.py

--- init.py
+++ init.py
@@ -1,1 +1,8 @@
+# --------------------------------------------------
+# init.py
+# Version: 1.0.0
+# Last Updated: Sep 4th, 2022
+# --------------------------------------------------
 
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./python')
